6611979a030342a6309a039a20d20b6a220175a2
Der TabellenDoktor <team@tabellendoktor.de>
2021-03-23 21:20:46 +0100

    comment corrected

--- 5_delete.sql
+++ 5_delete.sql
@@ -4,4 +4,4 @@
 -- This file delets some entries from the database without droping the whole tables
 
 DELETE FROM Tracks WHERE Album_ID = 8980 AND Track > 1; -- delete all tracks above 1 from the album with ID 8980;
-DELETE FROM Tracks WHERE Track_name LIKE '%Physical%'; -- deletes all tracks where the name contains Cool
+DELETE FROM Tracks WHERE Track_name LIKE '%Physical%'; -- deletes all tracks where the name contains "Physical"
